83bd2dd3cbbad3f4da31e1415b5eab181f37866e
TR-1000 <49962746+TR-1000@users.noreply.github.com>
2020-09-14 23:30:32 -0500

    Update Main.js

--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -63,70 +63,70 @@ fetchMovies = () => {
 }
 
 //Create New post
-  handleCreate = (createData) => {
-    fetch('/movies', {
-      body: JSON.stringify(createData),
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(createdPost => {
-      return createdPost.json()
-    })
-    .then(jsonedPost => {
+handleCreate = (createData) => {
+  fetch('/movies', {
+    body: JSON.stringify(createData),
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(createdPost => {
+    return createdPost.json()
+  })
+  .then(jsonedPost => {
     // change to home view
+this.props.handleView('home')
+    this.setState(prevState => {
+prevState.movies.push(jsonedPost)
+return { movies: prevState.movies }
+})
+  })
+.catch(err => console.log(err))
+}
+
+//update post
+
+handleUpdate = (updateData) => {
+  fetch(`/movies/${updateData.id}`, {
+    body: JSON.stringify(updateData),
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(updatedMovies => {
+      // switch back to the home view after editing a post
       this.props.handleView('home')
-      this.setState(prevState => {
-        prevState.movies.push(jsonedPost)
-        return { movies: prevState.movies }
-      })
+      // for simplicity's sake, we'll just make an extra AJAX call to automatically load the post this time!
+      // if you're up for a challenge though, try and see if you can figure out how to do it without an extra call
+      this.fetchMovies()
     })
     .catch(err => console.log(err))
-  }
-
-  //update post
-  handleUpdate = (updateData) => {
-    fetch(`/movies/${updateData.id}`, {
-      body: JSON.stringify(updateData),
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(updatedMovies => {
-        // switch back to the home view after editing a post
-        this.props.handleView('home')
-        // for simplicity's sake, we'll just make an extra AJAX call to automatically load the post this time!
-        // if you're up for a challenge though, try and see if you can figure out how to do it without an extra call
-        this.fetchMovies()
-      })
-      .catch(err => console.log(err))
-    }
+}
 
   componentDidMount() {
-    this.fetchMovies()
-  }
+  this.fetchMovies()
+}
 
-  handleDelete = (id) => {
-    fetch(`/movies/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(json => {
-        this.setState(prevState => {
-          const movies = prevState.movies.filter(movies => movies.id !== id)
-          return { movies }
-        })
-      })
-      .catch(err => console.log(err))
+handleDelete = (id) => {
+  fetch(`/movies/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
     }
-
+  })
+    .then(json => {
+      this.setState(prevState => {
+    const movies = prevState.movies.filter(movies => movies.id !== id)
+    return { movies }
+  })
+    })
+    .catch(err => console.log(err))
+}
   // ==============
   // RENDER
   // ==============
@@ -177,12 +177,6 @@ fetchMovies = () => {
         />
 
         }
-        handleCreate={this.handleCreate}
-        handleUpdate={this.handleUpdate}
-        formInputs={this.props.formInputs}
-        view={this.props.view}
-      />
-      }
 
       </main>
     )
