1944adb1ddf9a0819ee5cdf54c88dc54d1fbe518
TR-1000 <49962746+TR-1000@users.noreply.github.com>
2020-09-14 22:53:12 -0500

    Update Header.js
    updated popcorn image

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -11,7 +11,7 @@ class Header extends React.Component {
   render () {
     return (
       <header>
-        <h1>What Are You Watching Tonight?   <img src="http://www.sclance.com/pngs/popcorn-kernel-png/popcorn_kernel_png_1100209.png" alt="popcorn" height="42" width="42" className="flicker-in-1"/></h1>
+        <h1>What Are You Watching Tonight?   <img src="https://i.dlpng.com/static/png/7070718_preview.png" alt="popcorn" height="42" width="42" className="flicker-in-1"/></h1>
 
       </header>
     )
