5e506b0308d17e5205eeca39dfa6c6d3d4ec78e9
TR-1000 <49962746+TR-1000@users.noreply.github.com>
2020-09-23 23:51:20 -0500

    Update Header.js

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -11,8 +11,7 @@ class Header extends React.Component {
   render () {
     return (
       <header>
-        <h1>What Are You Watching Tonight?   <img src="https://i.dlpng.com/static/png/7070718_preview.png" alt="popcorn" height="42" width="42" className="flicker-in-1"/></h1>
-
+        <h1>What Are You Watching Tonight?  <img src="https://raw.githubusercontent.com/TR-1000/probable-potato/master/app/assets/images/popcorn.png" alt="popcorn" height="42" width="42" className="flicker-in-1"/></h1>
       </header>
     )
   }
